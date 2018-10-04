6ad5131072375fb93fc574a5accb47d11a09bd3f
Oscar Nylander <oscar@hedvig.com>
2018-10-04 13:58:50 +0200

    Sync schema on boot

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,5 @@
 import { createHttpLink } from 'apollo-link-http'
-import { buildClientSchema } from 'graphql'
+import { buildClientSchema, introspectionQuery } from 'graphql'
 import gql from 'graphql-tag'
 import {
   makeExecutableSchema,
@@ -8,27 +8,41 @@ import {
 } from 'graphql-tools'
 import fetch from 'node-fetch'
 
-import translationSchema = require('./external-schemas/translations.json')
 import { resolvers } from './resolvers'
 import { typeDefs } from './typeDefs'
 
-const translationsLink = createHttpLink({
-  uri: 'https://api-euwest.graphcms.com/v1/cjmawd9hw036a01cuzmjhplka/master',
-  fetch: fetch as any,
-})
+const makeSchema = async () => {
+  const translationSchema = await fetch(
+    'https://api-euwest.graphcms.com/v1/cjmawd9hw036a01cuzmjhplka/master',
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: introspectionQuery }),
+    },
+  ).then((res) => res.json())
+  const translationsLink = createHttpLink({
+    uri: 'https://api-euwest.graphcms.com/v1/cjmawd9hw036a01cuzmjhplka/master',
+    fetch: fetch as any,
+  })
 
-const executableTranslationsSchema = makeRemoteExecutableSchema({
-  schema: buildClientSchema(translationSchema as any),
-  link: translationsLink,
-})
+  const executableTranslationsSchema = makeRemoteExecutableSchema({
+    schema: buildClientSchema(translationSchema.data),
+    link: translationsLink,
+  })
 
-const localSchema = makeExecutableSchema({
-  typeDefs: gql(typeDefs),
-  resolvers,
-})
+  const localSchema = makeExecutableSchema({
+    typeDefs: gql(typeDefs),
+    resolvers,
+  })
 
-const schema = mergeSchemas({
-  schemas: [executableTranslationsSchema, localSchema],
-})
+  const schema = mergeSchemas({
+    schemas: [executableTranslationsSchema, localSchema],
+  })
 
-export { schema }
+  return schema
+}
+
+export { makeSchema }
