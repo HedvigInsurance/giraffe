52498d25befcc1c299a37027a0c23cac45b233cc
Oscar Nylander <oscar@hedvig.com>
2018-10-04 14:03:43 +0200

    I for Improvements

--- src/schema.ts
+++ src/schema.ts
@@ -1,35 +1,25 @@
 import { createHttpLink } from 'apollo-link-http'
-import { buildClientSchema, introspectionQuery } from 'graphql'
 import gql from 'graphql-tag'
 import {
+  introspectSchema,
   makeExecutableSchema,
   makeRemoteExecutableSchema,
   mergeSchemas,
 } from 'graphql-tools'
 import fetch from 'node-fetch'
-
 import { resolvers } from './resolvers'
 import { typeDefs } from './typeDefs'
 
 const makeSchema = async () => {
-  const translationSchema = await fetch(
-    'https://api-euwest.graphcms.com/v1/cjmawd9hw036a01cuzmjhplka/master',
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ query: introspectionQuery }),
-    },
-  ).then((res) => res.json())
   const translationsLink = createHttpLink({
     uri: 'https://api-euwest.graphcms.com/v1/cjmawd9hw036a01cuzmjhplka/master',
     fetch: fetch as any,
   })
 
+  const translationSchema = await introspectSchema(translationsLink)
+
   const executableTranslationsSchema = makeRemoteExecutableSchema({
-    schema: buildClientSchema(translationSchema.data),
+    schema: translationSchema,
     link: translationsLink,
   })
 
