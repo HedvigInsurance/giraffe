9fa54ae1f3d289c3531e16e121ff4e26b95519b8
Oscar Nylander <oscar@hedvig.com>
2018-10-04 11:33:27 +0200

    create offer wip

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,20 +1,45 @@
 import fetch from 'node-fetch'
+import {
+  InsuranceStatus,
+  InsuranceType,
+  PerilCategory,
+} from '../typings/generated-graphql-types'
+
+interface InsuranceDto {
+  currentTotalPrice: number
+  addressStreet: string
+  certificateUrl: string
+  status: InsuranceStatus
+  insuranceType: InsuranceType
+  activeFrom: string
+  categories: PerilCategory[]
+}
+
+const headers = (token: string) => ({
+  Authorization: `Bearer ${token}`,
+})
 
 const register = (baseUrl: string) => async () => {
   const data = await fetch(`${baseUrl}/helloHedvig`, { method: 'POST' })
   return data.text()
 }
 
-const getInsurance = (baseUrl: string) => async (token: string) => {
+const getInsurance = (baseUrl: string) => async (
+  token: string,
+): Promise<InsuranceDto> => {
   const data = await fetch(`${baseUrl}/insurance`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: headers(token),
   })
   return data.json()
 }
 
-const getUser = (baseUrl: string) => async (token: string) => {
+const getUser = (baseUrl: string) => async (
+  token: string,
+): Promise<{
+  safetyIncreasers: string[]
+}> => {
   const data = await fetch(`${baseUrl}/member/me`, {
-    headers: { Authorization: `Bearer ${token}` },
+    headers: headers(token),
   })
   return data.json()
 }
@@ -22,8 +47,16 @@ const getUser = (baseUrl: string) => async (token: string) => {
 const logoutUser = (baseUrl: string) => async (token: string) => {
   await fetch(`${baseUrl}/logout`, {
     method: 'POST',
-    headers: { Authorization: `Bearer ${token}` },
+    headers: headers(token),
   })
 }
 
-export { getInsurance, getUser, logoutUser, register }
+const createProduct = (baseUrl: string) => async (token: string, body: any) => {
+  await fetch(`${baseUrl}/insurance/createProductWeb`, {
+    method: 'POST',
+    headers: { ...headers(token), 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  })
+}
+
+export { getInsurance, getUser, logoutUser, register, createProduct }
