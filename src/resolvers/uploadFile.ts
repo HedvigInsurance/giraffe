fa0c0ef5ee284ed1de512eb68d884e53d02e92e7
Sam Pettersson <sam@sampettersson.com>
2018-10-15 10:36:08 +0200

    Add config example and get env vars from config.ts

--- src/resolvers/uploadFile.ts
+++ src/resolvers/uploadFile.ts
@@ -3,14 +3,19 @@ import { UserInputError } from 'apollo-server-koa'
 import * as cloudinary from 'cloudinary'
 
 import {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_CLOUD_NAME,
+} from '../config'
+import {
   File,
   MutationToUploadFileResolver,
 } from '../typings/generated-graphql-types'
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || '',
-  api_key: process.env.CLOUDINARY_API_KEY || '',
-  api_secret: process.env.CLOUDINARY_API_SECRET || '',
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_API_SECRET,
 })
 
 export const uploadFile: MutationToUploadFileResolver = async (
