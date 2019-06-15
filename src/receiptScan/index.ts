613c6e892be3c8b8b8bda410b6d3d9734e09013b
Gustaf Gunér <gustaf@guner.se>
2019-06-15 14:29:02 +0200

    OCR recepit scan WIP

--- src/receiptScan/index.ts
+++ src/receiptScan/index.ts
@@ -1,27 +1,3 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-import { ApolloServer } from 'apollo-server-koa'
-import * as Koa from 'koa'
-import * as compress from 'koa-compress'
-
-import { execute, GraphQLError, subscribe } from 'graphql'
-import { createServer } from 'http'
-import { SubscriptionServer } from 'subscriptions-transport-ws'
-import * as config from './config'
-import { getWebContext, getWebSocketContext } from './context'
-import { loggingMiddleware } from './middlewares/logger'
-import { makeSchema } from './schema'
-import { factory } from './utils/log'
-
-import * as Sentry from '@sentry/node'
-import { getInnerErrorsFromCombinedError } from './utils/graphql-error'
-Sentry.init({
-  dsn: config.SENTRY_DSN,
-  enabled: Boolean(config.SENTRY_DSN),
-  environment: config.SENTRY_ENV,
-})
-
 interface Rectangle {
   x: number
   y: number
@@ -46,7 +22,7 @@ interface ReceiptData {
 const totalLabels = ['total', 'totalt', 'betala']
 // const currencyLabels = ['sek', 'kr', 'eur', '€', 'usd', '$']
 
-const parseReceipt: (visionObject: any) => ReceiptData? = (
+const parseReceipt: (visionObject: any) => ReceiptData | null = (
   visionObject: any,
 ) => {
   const textAnnotations = visionObject[0].textAnnotations
@@ -153,6 +129,7 @@ const fs = require('fs')
 const results = JSON.parse(
   fs.readFileSync(__dirname + '/receipt-dump-1.json').toString(),
 )
+*/
 const vision = require('@google-cloud/vision')
 const client = new vision.ImageAnnotatorClient()
 client
@@ -165,65 +142,3 @@ client
   .catch((error: any) => {
     console.error(error)
   })
-*/
-const logger = factory.getLogger('index')
-
-const handleError = (error: GraphQLError): void => {
-  logger.error(
-    'Uncaught error in GraphQL. Original error: ',
-    error.originalError,
-  )
-
-  getInnerErrorsFromCombinedError(error).forEach((err) =>
-    logger.error('Inner error: ', err),
-  )
-}
-makeSchema().then((schema) => {
-  const server = new ApolloServer({
-    schema,
-    context: getWebContext,
-    playground: config.PLAYGROUND_ENABLED && {
-      subscriptionEndpoint: '/subscriptions',
-    },
-    introspection: true,
-    formatError: (error: GraphQLError) => {
-      handleError(error)
-      return error
-    },
-    engine: config.APOLLO_ENGINE_KEY
-      ? {
-          apiKey: config.APOLLO_ENGINE_KEY,
-        }
-      : undefined,
-  })
-  const app = new Koa()
-
-  app.use(compress())
-  app.use(loggingMiddleware)
-  server.applyMiddleware({ app })
-
-  const ws = createServer(app.callback())
-
-  ws.listen(config.PORT, () => {
-    logger.info(`Server listening at http://localhost:${config.PORT}`)
-    new SubscriptionServer( // tslint:disable-line no-unused-expression
-      {
-        keepAlive: 10_000,
-        execute,
-        subscribe,
-        schema,
-        onConnect: getWebSocketContext,
-        onOperation: (_msg: any, params: any) => {
-          params.formatResponse = (response: any) => {
-            if (response.errors) {
-              response.errors.forEach(handleError)
-            }
-            return response
-          }
-          return params
-        },
-      },
-      { server: ws, path: '/subscriptions' },
-    )
-  })
-})
