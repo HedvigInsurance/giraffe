ab05d9b6ccb8f36d84f1500a6d554364715a78e4
Oscar Nylander <oscar@hedvig.com>
2020-03-02 14:09:04 +0100

    More key gear item categories

--- src/resolvers/cross-schema/index.ts
+++ src/resolvers/cross-schema/index.ts
@@ -30,7 +30,7 @@ const getCoveredIds = (category: KeyGearItemCategory) => {
       'ck6rzeuhvc2fx0b20e6hpikkn',
       'ck6rzf86hc5s10b75c2yhalsw',
     ],
-    [KeyGearItemCategory.BICYCLE]: [
+    [KeyGearItemCategory.BIKE]: [
       'ck6rzgib1c2q10b20ctos0rt2',
       'ck6rzh304c6530b758ptd5wuu',
       'ck6rzhd2bc2uy0b200q3fz289',
@@ -43,6 +43,20 @@ const getCoveredIds = (category: KeyGearItemCategory) => {
     [KeyGearItemCategory.JEWELRY]: [
       'ck6rzld4ic80e0b75ebn58utz',
       'ck6rzln3gc3og0b209k5nwz15',
+    ],
+    [KeyGearItemCategory.SMART_WATCH]: [
+      'ck7abtsggljcj0b20rtllarey',
+      'ck7abuo6edohp0b844m7k4vkv',
+      'ck7abv5m9ljik0b206t1akm5m',
+      'ck7abvklydooi0b843y95xs4d',
+      'ck7abw0ucljnt0b20fumkjpdn',
+    ],
+    [KeyGearItemCategory.TABLET]: [
+      'ck7ah8icam1cr0b20c19fl08r',
+      'ck7ah98ose8qm0b846vqqemvq',
+      'ck7ah9mdhe8s40b84nk7ty9i0',
+      'ck7ah9z3am1fv0b20bkku4fmn',
+      'ck7ahadete8uq0b84pnxyf32j',
     ],
   }[category]
 }
@@ -61,7 +75,7 @@ const getExceptionIds = (category: KeyGearItemCategory) => {
       'ck6rzfmswc2kq0b20351aexv7',
       'ck6rzfzsyc5x40b7503pwcpvz',
     ],
-    [KeyGearItemCategory.BICYCLE]: [
+    [KeyGearItemCategory.BIKE]: [
       'ck6rzi60sc2z50b2066qisb0u',
       'ck6rzio31c30q0b20wtku0rhm',
       'ck6rziz5lc31t0b208u7i599h',
@@ -74,6 +88,14 @@ const getExceptionIds = (category: KeyGearItemCategory) => {
       'ck6rzm4f5c83u0b75uis9jhha',
       'ck6rzmfxhc85e0b75odf6gpkq',
     ],
+    [KeyGearItemCategory.SMART_WATCH]: [
+      'ck7abx2amdotb0b849boxdj9y',
+      'ck7abxjesdovd0b84mflswsel',
+    ],
+    [KeyGearItemCategory.TABLET]: [
+      'ck7ahasnam1hy0b20442td155',
+      'ck7ahb6xre8z10b84b39lvsju',
+    ],
   }[category]
 }
 
@@ -81,9 +103,11 @@ enum KeyGearItemCategory {
   PHONE = 'PHONE',
   COMPUTER = 'COMPUTER',
   TV = 'TV',
-  BICYCLE = 'BICYCLE',
-  WATCH = 'WATCH',
+  BIKE = 'BIKE',
   JEWELRY = 'JEWELRY',
+  WATCH = 'WATCH',
+  SMART_WATCH = 'SMART_WATCH',
+  TABLET = 'TABLET',
 }
 
 interface KeyGearItem {
