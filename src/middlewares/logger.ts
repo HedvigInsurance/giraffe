e4091992afc667a4ddb866ce85341fe498b26af8
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2020-04-07 08:05:27 +0200

    revert console log so we can deploy to prod

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -11,9 +11,6 @@ const loggingMiddleware = async (
   const { request, response } = ctx
 
   logger.info(`${request.method} '${request.url}' ${response.status}`)
-  if (request.url != '/.well-known/apollo/server-health') {
-    console.log('Body: ' + response.body)
-  }
 }
 
 export { loggingMiddleware }
