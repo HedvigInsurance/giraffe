48be6507496cffab4eba2e8de373e295a8dc6976
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-05-03 17:17:10 +0200

    remove comment

--- src/api/upstreams/productPricing.ts
+++ src/api/upstreams/productPricing.ts
@@ -16,30 +16,6 @@ export interface SelfChangeEligibilityDto {
   blockers: string[]
 }
 
-/*
-data class Contract(
-  val id: UUID,
-  val holderMemberId: String,
-  val switchedFrom: String?,
-  val masterInception: LocalDate?,
-  val status: ContractStatus,
-  val typeOfContract: TypeOfContract,
-  @get:JsonProperty("isTerminated")
-  val isTerminated: Boolean,
-  val terminationDate: LocalDate?,
-  val currentAgreementId: UUID,
-  val hasPendingAgreement: Boolean,
-  val agreements: List<Agreement>,
-  val genericAgreements: List<GenericAgreement>,
-  val hasQueuedRenewal: Boolean,
-  val renewal: Renewal?,
-  val preferredCurrency: CurrencyUnit,
-  val market: Market,
-  val signSource: SignSource?,
-  val contractTypeName: String,
-  val createdAt: Instant
-)
-*/
 export interface ContractDto {
   id: string
   holderMemberId: string
